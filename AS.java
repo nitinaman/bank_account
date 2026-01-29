cde7fe53a2906ec902a5c8e9b8b3de6f03212698
nitinaman <nitinaman3@gmail.com>
2026-01-29 21:31:40 +0530

    Create AS.java

---
+++ AS.java
@@ -0,0 +1,9 @@
+public class AS {
+    public static void main(String[] args) {
+
+        int side = 5;   // change value as needed
+        int area = side * side;
+
+        System.out.println("Area of the square is: " + area);
+    }
+}
