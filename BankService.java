16c2b8c48685698aee5cdc274301a8b1696a844f
nitinaman <nitinaman3@gmail.com>
2026-01-29 21:05:00 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -33,7 +33,7 @@ public class BankService {
     // The main entry point
     public static void main(String[] args) {
         // Create an instance of the BankAccount class
-        BankAccount acc = new BankAccount(5000);
+        BankAccount acc = new BankAccount(6000);
         
         System.out.println("Initial Balance: " + acc.getBalance());
 
